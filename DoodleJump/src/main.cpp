4331b34945b52eb906a7c59ee65ef4df221de042
baesparza <bruno.be81@gmail.com>
2018-02-08 21:59:36 -0500

    feat(DoodleJump): add lib SFML

--- DoodleJump/src/main.cpp
+++ DoodleJump/src/main.cpp
@@ -1,8 +1,30 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main() 
+int main()
 {
-	std::cout << "Hola" << std::endl;
-	std::cin.get();
+	sf::RenderWindow window(sf::VideoMode(800, 600),
+		"Hello SFML", sf::Style::Default);
+
+	sf::Font font;
+	font.loadFromFile("C:/Windows/Fonts/Arial.ttf");
+
+	sf::Text text;
+	text.setFont(font);
+	text.setPosition(200, 200);
+	text.setString("Hello SFML");
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		window.clear();
+		window.draw(text);
+		window.display();
+	}
+
 	return 0;
 }

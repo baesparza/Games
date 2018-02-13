8c2d181ad3bb75fdecc4a36fe330a313d238fe22
baesparza <bruno.be81@gmail.com>
2018-02-13 16:23:32 -0500

    feat(mines) add counter for mines

--- 05_Minesweeper/src/minesweeper.cpp
+++ 05_Minesweeper/src/minesweeper.cpp
@@ -3,24 +3,48 @@
 
 using namespace sf;
 
+
 int main()
 {
-	RenderWindow app(VideoMode(320, 320), "MinesWeeper");
-
-	int w = 32;
-	int grid[10][10], // 12 is used for offset
-		sgrid[10][10]; // for showing
+	RenderWindow app(VideoMode(400, 400), "MinesWeeper");
 
 	Texture t;
 	t.loadFromFile("./assets/images/tiles.jpg");
 
 	Sprite s(t);
 
+	unsigned const int w = 32; // size of each square
+	int grid[12][12],
+		sgrid[12][12]; // for showing
 
-	// fill grid with boid squares
-	for (int i = 0; i < 10; i++)
-		for (int j = 0; j < 10; j++)
+		// fill grid with boid squares
+	for (int i = 1; i <= 10; i++)
+		for (int j = 1; j <= 10; j++)
+		{
 			sgrid[i][j] = 10;
+			grid[i][j] = (rand() % 5 == 0) ? 9 : 0; // fill with mines or safe
+		}
+
+	// know num of mines touching each square
+	for (int i = 1; i <= 10; i++)
+		for (int j = 1; j <= 10; j++)
+		{
+			if (grid[i][j] == 9) continue;
+
+			// counter for mines
+			int n = 0;
+			if (grid[i - 1][j - 1] == 9) n++; // top left
+			if (grid[i - 1][j] == 9) n++; // top center
+			if (grid[i - 1][j + 1] == 9) n++; // top right
+			if (grid[i][j + 1] == 9) n++; // right center
+			if (grid[i + 1][j + 1] == 9) n++; // bottom right
+			if (grid[i + 1][j] == 9) n++; // bottom center
+			if (grid[i + 1][j - 1] == 9) n++; // bottom left
+			if (grid[i][j - 1] == 9) n++; // left center
+
+			grid[i][j] = n;
+		}
+
 
 
 	while (app.isOpen())
@@ -34,9 +58,10 @@ int main()
 
 		app.clear(Color::White);
 
-		for (int i = 0; i < 10; i++)
-			for (int j = 0; j < 10; j++)
+		for (int i = 1; i <= 10; i++)
+			for (int j = 1; j <= 10; j++)
 			{
+				sgrid[i][j] = grid[i][j];
 				s.setTextureRect(IntRect(sgrid[i][j] * w, 0, w, w));
 				s.setPosition(i * w, j * w);
 				app.draw(s);

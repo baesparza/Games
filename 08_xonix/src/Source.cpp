1e357c2a9c3f99c9197fa6c946c8cd15fdab1502
baesparza <bruno.be81@gmail.com>
2018-02-18 13:51:38 -0500

    feat(grid): draw grid

--- 08_xonix/src/Source.cpp
+++ 08_xonix/src/Source.cpp
@@ -5,7 +5,7 @@ using namespace sf;
 
 const unsigned int M = 20, N = 40;
 
-int grif[M][N];
+int grid[M][N];
 const unsigned int TS = 18; // tile size
 
 
@@ -25,6 +25,12 @@ int main()
 	int x = 0, y = 0, dx = 0, dy = 0;
 	float timer = 0.f, delay = 0.07f;
 	Clock clock;
+
+	// fill border
+	for (int i = 0; i < M; i++)
+		for (int j = 0; j < N; j++)
+			grid[i][j] = (i == 0 || j == 0 || i == M - 1 || j == N - 1) ? 1 : 0;
+
 
 	while (window.isOpen())
 	{
@@ -50,12 +56,33 @@ int main()
 		{
 			x += dx;
 			y += dy;
+
+			// def limit
+			if (x < 0) x = 0;
+			if (x > N - 1) x = N - 1;
+			if (y < 0) y = 0;
+			if (y > M - 1) y = M - 1;
+
+			// define this tile as used
+			if (grid[y][x] == 0) grid[y][x] = 2;
+
 			timer = 0;
 		}
 
 		////////draw////////////
 		window.clear(Color::Black);
+		// grig
+		for (int i = 0; i < M; i++)
+			for (int j = 0; j < N; j++)
+			{
+				if (grid[i][j] == 0) continue;
+				else if (grid[i][j] == 1) sTile.setTextureRect(IntRect(0, 0, TS, TS));
+				else if (grid[i][j] == 2) sTile.setTextureRect(IntRect(54, 0, TS, TS));
+				sTile.setPosition(j * TS, i * TS);
+				window.draw(sTile);
+			}
 
+		// main tile
 		sTile.setTextureRect(IntRect(36, 0, TS, TS));
 		sTile.setPosition(x * TS, y * TS);
 		window.draw(sTile);

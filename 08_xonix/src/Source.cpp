64d113bd0ba453e4f0915784de0747036437ecc1
baesparza <bruno.be81@gmail.com>
2018-02-18 13:43:05 -0500

    feat(pointer): add main tile

--- 08_xonix/src/Source.cpp
+++ 08_xonix/src/Source.cpp
@@ -1,28 +1,65 @@
 #include <SFML/Graphics.hpp>
+#include <time.h>
+
+using namespace sf;
+
+const unsigned int M = 20, N = 40;
+
+int grif[M][N];
+const unsigned int TS = 18; // tile size
+
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(800, 600),
-		"Hello SFML", sf::Style::Default);
+	srand(time(0));
 
-	sf::Font font;
-	font.loadFromFile("C:/Windows/Fonts/Arial.ttf");
+	RenderWindow window(VideoMode(N * TS, M * TS), "XONIX");
 
-	sf::Text text;
-	text.setFont(font);
-	text.setPosition(200, 200);
-	text.setString("Bruno Esparza");
+	// load texture
+	Texture t1;
+	t1.loadFromFile("./assets/images/tiles.png");
+
+	Sprite sTile(t1);
+
+
+	int x = 0, y = 0, dx = 0, dy = 0;
+	float timer = 0.f, delay = 0.07f;
+	Clock clock;
 
 	while (window.isOpen())
 	{
-		sf::Event event;
-		while (window.pollEvent(event))
+		float time = clock.getElapsedTime().asSeconds();
+		clock.restart();
+		timer += time;
+
+		Event e;
+		while (window.pollEvent(e))
 		{
-			if (event.type == sf::Event::Closed)
+			if (e.type == Event::Closed)
 				window.close();
 		}
-		window.clear();
-		window.draw(text);
+
+
+		// keyboar input
+		if (Keyboard::isKeyPressed(Keyboard::Left)) { dx = -1; dy = 0; }
+		else if (Keyboard::isKeyPressed(Keyboard::Right)) { dx = 1; dy = 0; }
+		else if (Keyboard::isKeyPressed(Keyboard::Up)) { dx = 0; dy = -1; }
+		else if (Keyboard::isKeyPressed(Keyboard::Down)) { dx = 0; dy = 1; }
+
+		if (timer > delay)
+		{
+			x += dx;
+			y += dy;
+			timer = 0;
+		}
+
+		////////draw////////////
+		window.clear(Color::Black);
+
+		sTile.setTextureRect(IntRect(36, 0, TS, TS));
+		sTile.setPosition(x * TS, y * TS);
+		window.draw(sTile);
+
 		window.display();
 	}
 	return 0;

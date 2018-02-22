310128d8a06f2226aa725ce83bbd8f8f5f0ec4f0
baesparza <bruno.be81@gmail.com>
2018-02-21 23:45:53 -0500

    feat(rotation): add rotation with mouse clicks

--- 11_NetWalk/src/Main.cpp
+++ 11_NetWalk/src/Main.cpp
@@ -10,12 +10,17 @@ Vector2f offset(65, 55);
 struct Pipe
 {
 	int orientation;
+	float angle;
 	bool on;
+
+	Pipe() : angle(0)
+	{ }
 } grid[N][N];
 
 int main()
 {
 	RenderWindow app(VideoMode(390, 390), "The Pipe Puzzle");
+	app.setFramerateLimit(60);
 
 	Texture t1, t2;
 	t1.loadFromFile("./images/background.png");
@@ -28,22 +33,40 @@ int main()
 	{
 		Event e;
 		while (app.pollEvent(e))
+		{
 			if (e.type == Event::Closed)
 				app.close();
 
+			if (e.type == Event::MouseButtonPressed)
+				if (e.key.code == Mouse::Left)
+				{
+					Vector2i pos = Mouse::getPosition(app) + Vector2i(TS / 2, TS / 2) - Vector2i(offset);
+					pos /= TS;
+					grid[pos.x][pos.y].orientation++;
+				}
+		}
+
+
+		/////draw/////
 		app.clear();
 		app.draw(sBackground);
-		
+
 		for (int i = 0; i < N; i++)
 			for (int j = 0; j < N; j++)
 			{
+				// smoth rotation
+				grid[j][i].angle += 5;
+				if (grid[j][i].angle > grid[j][i].orientation * 90)
+					grid[j][i].angle = grid[j][i].orientation * 90;
+
+				// draw pipe
 				sPipe.setTextureRect(IntRect(TS * 2, 0, TS, TS));
-				sPipe.setRotation(grid[j][i].orientation);
+				sPipe.setRotation(grid[j][i].angle);
 				sPipe.setPosition(j * TS, i * TS);
 				sPipe.move(offset);
 				app.draw(sPipe);
 			}
-		
+
 		app.display();
 	}
 

51bcbd71b922ebbda88a538765d7d337892ff99b
baesparza <bruno.be81@gmail.com>
2018-03-05 11:46:15 -0500

    feat(pieces): add shuffle

--- 12_Mahjong_Solitarie/src/Main.cpp
+++ 12_Mahjong_Solitarie/src/Main.cpp
@@ -14,6 +14,21 @@ int & f(sf::Vector3i v)
 	return f(v.x, v.y, v.z);
 }
 
+bool isOpen(int x, int y, int z)
+{
+	for (int i = -1; i <= 1; i++)
+		for (int j = -1; j <= 1; j++)
+			if (f(x + 2, y + i, z) > 0 && f(x - 2, y + j, z) > 0)
+				return false;
+
+	for (int i = -1; i <= 1; i++)
+		for (int j = -1; j <= 1; j++)
+			if (f(x + i, y + j, z + 1) > 0)
+				return false;
+
+	return true;
+}
+
 int main()
 {
 	sf::RenderWindow app(sf::VideoMode(740, 570), "Mahjong Solitarie");
@@ -26,6 +41,7 @@ int main()
 
 	const int W = 48, H = 66;
 	const int stepX = W / 2 - 2, stepY = H / 2 - 2;
+	const float offX = 4.6f, offY = 7.1f; // z offset 
 
 	///////////load from file//////
 	std::fstream file("./assets/map.txt");
@@ -41,6 +57,36 @@ int main()
 				else
 					f(x, y, z) = 1;
 		}
+
+	//////shufle//////
+	for (int k = 1; ; k++)
+	{
+		std::vector<sf::Vector3i> opens;
+		for (int z = 0; z < 10; z++)
+			for (int y = 0; y < 18; y++)
+				for (int x = 0; x < 30; x++)
+					if (f(x, y, z) > 0 && isOpen(x, y, z))
+						opens.push_back(sf::Vector3i(x, y, z));
+
+		int n = opens.size();
+		if (n < 2)
+			break;
+		int a = 0, b = 0;
+		while (a == b)
+		{
+			a = rand() % n;
+			b = rand() % n;
+		}
+		f(opens[a]) = -k;
+		f(opens[b]) = -k;
+		k %= 42;
+	}
+
+	for (int z = 0; z < 10; z++)
+		for (int y = 0; y < 18; y++)
+			for (int x = 0; x < 30; x++)
+				f(x, y, z) *= -1;
+
 
 	while (app.isOpen())
 	{
@@ -63,7 +109,7 @@ int main()
 					if (k == 0)
 						continue;
 					sTile.setTextureRect(sf::IntRect(k * W, 0, W, H));
-					sTile.setPosition(x * stepX, y * stepY);
+					sTile.setPosition(x * stepX + z * offX, y * stepY - z * offY);
 
 					app.draw(sTile);
 				}

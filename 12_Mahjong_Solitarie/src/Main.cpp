d4bea40a0b659b99d2b48e438ae1e67f4b104c8c
baesparza <bruno.be81@gmail.com>
2018-03-05 11:08:34 -0500

    feat(file): add file stream

--- 12_Mahjong_Solitarie/src/Main.cpp
+++ 12_Mahjong_Solitarie/src/Main.cpp
@@ -2,6 +2,18 @@
 #include<fstream>
 #include <SFML/Graphics.hpp>
 
+int field[50][50][50] = {0};
+
+int & f(int x, int y, int z)
+{
+	return field[y + 2][x + 2][z];
+}
+
+int & f(sf::Vector3i v)
+{
+	return f(v.x, v.y, v.z);
+}
+
 int main()
 {
 	sf::RenderWindow app(sf::VideoMode(740, 570), "Mahjong Solitarie");
@@ -11,6 +23,24 @@ int main()
 	t2.loadFromFile("./assets/images/tiles.png");
 
 	sf::Sprite sBackground(t1), sTile(t2);
+
+	const int W = 48, H = 66;
+	const int stepX = W / 2 - 2, stepY = H / 2 - 2;
+
+	///////////load from file//////
+	std::fstream file("./assets/map.txt");
+	for (int y = 0; y < 18; y++)
+		for (int x = 0; x < 30; x++)
+		{
+			char a;
+			file >> a;
+			int n = a - '0';
+			for (int z = 0; z < n; z++)
+				if (f(x - 1, y - 1, z))
+					f(x - 1, y, z) = f(x, y - 1, z) = 0;
+				else
+					f(x, y, z) = 1;
+		}
 
 	while (app.isOpen())
 	{
@@ -25,7 +55,19 @@ int main()
 		app.clear();
 		app.draw(sBackground);
 
-		app.display();
+		for (int z = 0; z < 10; z++)
+			for (int x = 30; x >= 0; x--)
+				for (int y = 0; y < 18; y++)
+				{
+					int k = f(x, y, z);
+					if (k == 0)
+						continue;
+					sTile.setTextureRect(sf::IntRect(k * W, 0, W, H));
+					sTile.setPosition(x * stepX, y * stepY);
+
+					app.draw(sTile);
+				}
+				app.display();
 	}
 
 	return 0;

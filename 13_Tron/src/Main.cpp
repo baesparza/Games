d526d139442393735e9b16a40b3b8b16b3e8f49f
baesparza <bruno.be81@gmail.com>
2018-03-05 19:04:27 -0500

    feat(tron): add game

--- 13_Tron/src/Main.cpp
+++ 13_Tron/src/Main.cpp
@@ -18,6 +18,11 @@ struct Player
 
 	void tick()
 	{
+		//		^
+		//		3
+		//	< 1 + 2 >
+		//		0
+		//		v
 		if (dir == 0) y += 1;
 		if (dir == 1) x -= 1;
 		if (dir == 2) x += 1;
@@ -28,14 +33,82 @@ struct Player
 		if (y >= H) y = 0;
 		if (y < 0) y = H - 1;
 	}
-
-	sf::Color & getColor()
-	{
-		return color;
-	}
 };
 
 int main()
 {
+	srand(time(0));
+
+	sf::RenderWindow app(sf::VideoMode(W, H), "TRON");
+	app.setFramerateLimit(60);
+
+	sf::Texture t1;
+	t1.loadFromFile("./assets/images/background.jpg");
+
+	sf::Sprite sBackground(t1);
+
+	Player p1(sf::Color::Blue), p2(sf::Color::Red);
+
+	sf::Sprite sprite;
+	sf::RenderTexture t;
+	t.create(W, H);
+	t.setSmooth(true);
+	sprite.setTexture(t.getTexture());
+	t.clear();
+	t.draw(sBackground);
+
+	bool game = true;
+
+	while (app.isOpen())
+	{
+		sf::Event e;
+		while (app.pollEvent(e))
+		{
+			using key = sf::Keyboard;
+
+			if (e.type == sf::Event::Closed) app.close();
+
+			// player 1
+			if (key::isKeyPressed(key::Left)) if (p1.dir != 2) p1.dir = 1;
+			if (key::isKeyPressed(key::Right)) if (p1.dir != 1) p1.dir = 2;
+			if (key::isKeyPressed(key::Up)) if (p1.dir != 0) p1.dir = 3;
+			if (key::isKeyPressed(key::Down)) if (p1.dir != 3) p1.dir = 0;
+
+			// player 2
+			if (key::isKeyPressed(key::A)) if (p2.dir != 2) p2.dir = 1;
+			if (key::isKeyPressed(key::D)) if (p2.dir != 1) p2.dir = 2;
+			if (key::isKeyPressed(key::W)) if (p2.dir != 0) p2.dir = 3;
+			if (key::isKeyPressed(key::S)) if (p2.dir != 3) p2.dir = 0;
+		}
+		
+		if (!game) continue; // when lose, stop game
+
+		for (int i = 0; i < speed; i++)
+		{
+			p1.tick();
+			p2.tick();
+
+			if (field[p1.x][p1.y] == 1 || field[p2.x][p2.y] == 1)
+				game = false;
+
+			field[p1.x][p1.y] = field[p2.x][p2.y] = 1;
+
+			sf::CircleShape c(3);
+			c.setPosition(p1.x, p1.y);
+			c.setFillColor(p1.color);
+			t.draw(c);
+			c.setPosition(p2.x, p2.y);
+			c.setFillColor(p2.color);
+			t.draw(c);
+
+			t.display();
+		}
+
+		/////////draw/////////
+		app.clear();
+		app.draw(sprite);
+		app.display();
+	}
+
 	return 0;
 }

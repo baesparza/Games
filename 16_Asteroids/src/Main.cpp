f69962831f16e01abcaa24d3a5a3b2493a176435
baesparza <bruno.be81@gmail.com>
2018-03-10 16:39:54 -0500

    feat(ship): add movement

--- 16_Asteroids/src/Main.cpp
+++ 16_Asteroids/src/Main.cpp
@@ -2,6 +2,8 @@
 
 const int W = 1200, H = 800;
 
+const float DEGTORAD = 0.017453f;
+
 int main()
 {
 	sf::RenderWindow app(sf::VideoMode(W, H), "Asteroids");
@@ -17,6 +19,12 @@ int main()
 	sPlayer.setTextureRect(sf::IntRect(40,0,40,40));
 	sPlayer.setOrigin(20, 20);
 
+	float x = 300, y = 300;
+	float dx = 0, dy = 0, angle = 0;
+	bool thrust;
+
+
+
 	while (app.isOpen())
 	{
 		sf::Event e;
@@ -26,6 +34,41 @@ int main()
 			if (e.type == sf::Event::Closed)
 				app.close();
 		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) angle += 3;
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) angle -= 3;
+		thrust = sf::Keyboard::isKeyPressed(sf::Keyboard::Up);
+
+		/////space ship movement/////
+		if (thrust)
+		{
+			dx += std::cos(angle * DEGTORAD) * 0.2;
+			dy += std::sin(angle * DEGTORAD) * 0.2;
+		}
+		else
+		{
+			dx *= 0.99;
+			dy *= 0.99;
+		}
+
+		int maxSpeed = 15;
+		float speed = std::sqrt(dx * dx + dy * dy);
+		if (speed > maxSpeed)
+		{
+			dx *= maxSpeed / speed;
+			dy *= maxSpeed / speed;
+		}
+
+		x += dx;
+		y += dy;
+
+		if (x > W) x = 0;
+		if (x < 0) x = W;
+		if (y > H) y = 0;
+		if (y < 0) y = H;
+
+		sPlayer.setPosition(x, y);
+		sPlayer.setRotation(angle + 90);
 
 		/////draw/////
 		app.clear();
